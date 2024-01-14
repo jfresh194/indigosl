31d09de46a93ce774ce9e3aa3a0d3b8bbdfecb08
indigosl <138515772+indigosl@users.noreply.github.com>
2024-01-14 08:32:24 +0000

    data.js

--- data.js
+++ data.js
@@ -1,1 +1,23 @@
 
+
+[
+    {
+    "hometeamname": "Nigeria",
+    "hometeamname": "Equatorial Guinea",
+    "awayteamname": "https://ssl.gstatic.com/onebox/media/sports/logos/2vpR8clrEZdpNjaiHKpg7A_96x96.png",
+    "awayteamflag": "https://ssl.gstatic.com/onebox/media/sports/logos/MgTgO44PlRIyAdPRGCPG0Q_96x96.png",
+    "leaguename": "AFCON",
+    "leagueimage": "https://cdn.livetv747.me/img/tables/311.gif",
+    "month": "1",
+    "date": "Sun",
+    "day": "14",
+    "timehour": "14",
+    "timemins": "00",
+    "matchlink": "https://livetv749.me/enx/eventinfo/183493510_nigeria_nga_equatorial_guinea_gnq"
+    },
+
+
+
+  
+  ]
+
